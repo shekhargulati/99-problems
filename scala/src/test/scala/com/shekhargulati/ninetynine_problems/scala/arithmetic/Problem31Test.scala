b0bdb10bbecb93278042364a62ebe965a973b8fb
Shekhar Gulati <shekhargulati84@gmail.com>
2016-01-23 16:32:37 +0530

    P31 added Trial division approach

--- scala/src/test/scala/com/shekhargulati/ninetynine_problems/scala/arithmetic/Problem31Test.scala
+++ scala/src/test/scala/com/shekhargulati/ninetynine_problems/scala/arithmetic/Problem31Test.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FunSpec, Matchers}
 
 class Problem31Test extends FunSpec with Matchers {
 
-  describe("Problem 29 Spec") {
+  describe("Problem 31 Spec") {
 
     it("7 is a prime number") {
       val prime = Problem31.isPrime(7)
@@ -15,6 +15,30 @@ class Problem31Test extends FunSpec with Matchers {
       val prime = Problem31.isPrime(10)
       prime should be(false)
     }
+  }
+
+  describe("Problem 31 Spec (trial division)") {
+    it("7 is a prime number") {
+      val prime = Problem31.isPrime_trialDivision(7)
+      prime should be(true)
+    }
+
+    it("10 is not prime number") {
+      val prime = Problem31.isPrime_trialDivision(10)
+      prime should be(false)
+    }
+
+    it("15,485,863 is last prime below one million") {
+      val prime = Problem31.isPrime_trialDivision(15485863)
+      prime should be(true)
+    }
+
+    it("86,028,121 is the last prime below five million") {
+      val i: Int = 86028121
+      val prime = Problem31.isPrime_trialDivision(86028121)
+      prime should be(true)
+    }
+
   }
 
 }
