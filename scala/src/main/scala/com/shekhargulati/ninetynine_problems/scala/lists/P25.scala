f4f508f5ee29cd2186361744a3673e8ef2ef6a3f
Shekhar Gulati <shekhargulati84@gmail.com>
2016-01-28 20:30:40 +0530

    orgainzed package structure so that problems are shown in correct order

--- scala/src/main/scala/com/shekhargulati/ninetynine_problems/scala/lists/P25.scala
+++
@@ -1,9 +0,0 @@
-package com.shekhargulati.ninetynine_problems.scala.lists
-
-/**
-  * (*) Generate a random permutation of the elements of a list.
-  */
-object P25 {
-
-  def randomPermutation[T](list: List[T]): List[T] = P23.randomSelectR(list, list.size)
-}
