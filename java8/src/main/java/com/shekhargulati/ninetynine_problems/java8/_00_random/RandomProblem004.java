93176c4307654515e1d2cec702e98d1851497ce8
Shekhar Gulati <shekhargulati84@gmail.com>
2016-02-12 10:03:43 +0530

    RandomProblem004.java simple math functions

---
+++ java8/src/main/java/com/shekhargulati/ninetynine_problems/java8/_00_random/RandomProblem004.java
@@ -0,0 +1,47 @@
+package com.shekhargulati.ninetynine_problems.java8._00_random;
+
+/**
+ * Functions corresponding to loops
+ */
+public class RandomProblem004 {
+
+    /**
+     * f(n) = n*(n+1)/2
+     */
+    public static int sumOfNNumbers(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+
+    /**
+     * f(n) = n*n
+     */
+    public static int squareOfANumber(int n) {
+        int square = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                square += 1;
+            }
+        }
+        return square;
+    }
+
+    /**
+     * f(n) = n*(n+1)/2
+     */
+    public static int sumOfNNumbers1(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sum += 1;
+            }
+        }
+
+        return sum;
+    }
+
+}
---
+++ java8/src/test/java/com/shekhargulati/ninetynine_problems/java8/_00_random/RandomProblem004Test.java
@@ -0,0 +1,28 @@
+package com.shekhargulati.ninetynine_problems.java8._00_random;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class RandomProblem004Test {
+
+    @Test
+    public void sumOfFirst10NumbersIs55() throws Exception {
+        int sumOfNNumbers = RandomProblem004.sumOfNNumbers(10);
+        assertThat(sumOfNNumbers, equalTo(55));
+    }
+
+    @Test
+    public void squareOf10Is100() throws Exception {
+        int square = RandomProblem004.squareOfANumber(10);
+        assertThat(square, equalTo(100));
+    }
+
+    @Test
+    public void sumOfFirst10NumbersIs55_1() throws Exception {
+        int sum = RandomProblem004.sumOfNNumbers1(10);
+        assertThat(sum, equalTo(55));
+
+    }
+}
