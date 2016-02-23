e35aff37994417c11053f27bbb28d87561985d54
Shekhar Gulati <shekhargulati84@gmail.com>
2016-02-23 22:17:41 +0530

    Binary Tree implementation

---
+++ java8/src/main/java/com/shekhargulati/ninetynine_problems/java8/_00_random/tree/Tree.java
@@ -0,0 +1,39 @@
+package com.shekhargulati.ninetynine_problems.java8._00_random.tree;
+
+import java.util.Objects;
+
+public class Tree<T> {
+
+    T item;
+    Tree<T> left;
+    Tree<T> right;
+    Tree<T> parent;
+}
+
+class TreeOperations {
+
+    public <T extends Comparable<? super T>> Tree<T> search(Tree<T> tree, T item) {
+        if (Objects.equals(tree.item, item)) {
+            return tree;
+        }
+        if (tree.item.compareTo(item) < 0) {
+            return search(tree.left, item);
+        } else {
+            return search(tree.right, item);
+        }
+    }
+
+    public <T> T min(Tree<T> tree) {
+        if (tree.left == null) {
+            return tree.item;
+        }
+        return min(tree.left);
+    }
+
+    public <T> T max(Tree<T> tree) {
+        if (tree.right == null) {
+            return tree.item;
+        }
+        return max(tree.right);
+    }
+}
