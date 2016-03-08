2b8562ef383df2d0b6cb0528207d3781ef183040
Shekhar Gulati <shekhargulati84@gmail.com>
2016-03-08 13:53:33 +0530

    Problem03 using StringBuilder

---
+++ java8/src/main/java/com/shekhargulati/leetcode/Problem03.java
@@ -0,0 +1,22 @@
+package com.shekhargulati.leetcode;
+
+/**
+ * Longest substring without repeating characters
+ */
+public class Problem03 {
+
+    public static String substring(final String input) {
+        StringBuilder subStr = new StringBuilder();
+        String[] chars = input.split("");
+        for (String ch : chars) {
+            int indexCh = subStr.indexOf(ch);
+            if (indexCh == -1) {
+                subStr.append(ch);
+            } else {
+                subStr = new StringBuilder(subStr.substring(indexCh + 1));
+                subStr.append(ch);
+            }
+        }
+        return subStr.toString();
+    }
+}
---
+++ java8/src/test/java/com/shekhargulati/leetcode/Problem03Test.java
@@ -0,0 +1,23 @@
+package com.shekhargulati.leetcode;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class Problem03Test {
+
+    @Test
+    public void shouldFindLongestSubstring() throws Exception {
+        final String input = "abcabc";
+        String substring = Problem03.substring(input);
+        assertThat(substring, equalTo("abc"));
+    }
+
+    @Test
+    public void shouldFindLongestSubstringIn_bbbbb() throws Exception {
+        final String input = "bbbb";
+        String substring = Problem03.substring(input);
+        assertThat(substring, equalTo("b"));
+    }
+}
