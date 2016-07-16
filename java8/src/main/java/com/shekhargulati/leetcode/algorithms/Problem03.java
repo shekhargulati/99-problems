e8b60a8afabe766a193927a30baac9941ba258ec
careyyu <carey_yu@126.com>
2016-07-16 21:32:59 +0800

    Update Problem03.java
    i think  the original algorithm don't work well on some contidion, like "xdfsqerr". i do some refine on the original.

--- java8/src/main/java/com/shekhargulati/leetcode/algorithms/Problem03.java
+++ java8/src/main/java/com/shekhargulati/leetcode/algorithms/Problem03.java
@@ -1,22 +1,54 @@
 package com.shekhargulati.leetcode.algorithms;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Longest substring without repeating characters
  */
 public class Problem03 {
 
-    public static String substring(final String input) {
-        StringBuilder subStr = new StringBuilder();
-        String[] chars = input.split("");
-        for (String ch : chars) {
-            int indexCh = subStr.indexOf(ch);
-            if (indexCh == -1) {
-                subStr.append(ch);
-            } else {
-                subStr = new StringBuilder(subStr.substring(indexCh + 1));
-                subStr.append(ch);
-            }
-        }
-        return subStr.toString();
-    }
+   public static String substring(final String input) {
+		Map<Integer, Integer> resultCache = new HashMap<Integer, Integer>();
+		int start = 0;
+		int curIndex = -1;
+		StringBuilder subStr = new StringBuilder();
+		String[] chars = input.split("");
+		for (String ch : chars) {
+			curIndex++;
+			int indexCh = subStr.indexOf(ch);
+			if (indexCh == -1) {
+				subStr.append(ch);
+			}
+			else {
+				int l = subStr.length();
+				subStr = new StringBuilder(subStr.substring(indexCh + 1));
+				subStr.append(ch);
+				start += indexCh + 1;
+			}
+			resultCache.put(start, curIndex);
+		}
+		return processResult(resultCache, chars);
+	}
+
+	public static String processResult(Map<Integer, Integer> result, String[] chars) {
+		int maxLength = 0;
+		int resultStart = 0;
+		int resultEnd = 0;
+		for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+			int start = entry.getKey().intValue();
+			int end = entry.getValue().intValue();
+			int length = end - start + 1;
+			if (length > maxLength) {
+				maxLength = length;
+				resultStart = start;
+				resultEnd = end;
+			}
+		}
+		StringBuffer sb = new StringBuffer();
+		for (int i = resultStart; i <= resultEnd; i++) {
+
+			sb.append(chars[i]);
+		}
+		return sb.toString();
+	}
 }
