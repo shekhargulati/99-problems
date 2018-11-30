81336ef32bdc15d61efdc875675a26d55edb2647
Dhwani Sondhi <dhwanisondhi3@gmail.com>
2018-11-30 04:00:56 -0500

    Updated JavaDoc of problem 57

--- java8/src/main/java/com/shekhargulati/ninetynine_problems/_04_binary_trees/P57.java
+++ java8/src/main/java/com/shekhargulati/ninetynine_problems/_04_binary_trees/P57.java
@@ -5,6 +5,12 @@ package com.shekhargulati.ninetynine_problems._04_binary_trees;
  */
 public class P57 {
 
+    /**
+     * Common Tree Class
+     *
+     * implements {@link Comparable}
+     * @param <T> extends {@link Comparable } the id for each node
+     */
     public static class Node<T extends Comparable<T>> implements Comparable<Node<T>>{
 
         T id;
@@ -14,6 +20,13 @@ public class P57 {
         public Node(T id){
             this.id=id;
         }
+
+        /**
+         * To compare two nodes
+         *
+         * @param node {@link Node} the instance of the node class
+         * @return the positive or negative value after comparing two nodes
+         */
         @Override
         public int compareTo(Node<T> node){
             if(this.id.compareTo(node.id)==0)   return 0;
@@ -21,6 +34,13 @@ public class P57 {
             return -1;
         }
     }
+    /**
+     * To count the leaves on a binary tree
+     *
+     * @param root {@link Node} the binary tree
+     * @param N the count of leaves
+     * @return integer the count of leaves
+     */
     public static int count_leaves(Node root, int N){
         if(root==null)     return N;
         else if(root.left==null && root.right==null)         N++;
